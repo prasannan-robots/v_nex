c12f3c59f3f08fb7e7bfffd88fa66dc9f68efb91
prasannan-robots <prasannanatarajan3595@gmail.com>
2022-03-24 22:16:09 +0530

    Done changes to get file needs testing

--- github_storage_system/github_api.py
+++ github_storage_system/github_api.py
@@ -18,45 +18,64 @@ class git_file_server:
     def add_to_update_file(self):
         for filename in os.scandir(self.path_of_upload_folder):
             if filename.is_file():
+                self.update_files.append(filename.path)
                 f = open(filename.path,"rb")
                 content = f.read()
-                self.repository.create_file(filename.path,f"Updated {filename.path}",content,branch="main")
+                self.repository.create_file(filename.path,f"Updated {filename.path}",content,branch=self.branch)
                 if os.path.exists(filename.path):
                     os.remove(filename.path)
-                
-        
-                
 
-
-
-class github_store:
-    def __init__(self,file_path=None):
-        if file_path == None:
-            self.file_path = "chat_log.txt"
-        self.token = os.getenv("GITHUB_TOKEN")
-        self.github_object = Github(self.token)
-        self.branch = "main"
-        self.repository = self.github_object.get_repo("marsha-nicky/my_knowledge")
+    # Intend to return an array of all files in image directory
+    def get_all_filename(self):
+        return self.repository.get_contents("images",ref=self.branch)
     
-    def pull_data(self):
-        contents = self.repository.get_contents(self.file_path, ref=self.branch)
+    # Intend to get file content from github
+    def get_file_content(self,filename):
+        contents = self.repository.get_contents(filename, ref=self.branch)
         return contents.decoded_content.decode()
+    
+    # Intend to get file and save it in image directory
+    def get_file(self,filename):
+        file_content=self.get_file_content(filename)
+        f=open(filename,"wb")
+        f.write(file_content)
+    
+    # Intend to download all files in image directory
+    def get_all_file(self):
+        for i in self.get_all_filename():
+            self.get_file(i)   
+                
 
-    def push(self,path=None, message=None, content=None, branch=None, update=False):
 
-        if path == None:
-            path = self.file_path
-        if message == None:
-            message = "Added chat knowledge"
-        if content == None:
-            content = self.data
-        if branch == None:
-            branch = self.branch
 
-        source = self.repository.get_branch(branch)
-        if update:  # If file already exists, update it
-            contents = self.repository.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
-            self.repository.update_file(contents.path, message, content, contents.sha, branch=branch)  # Add, commit and push branch
-            print("github_storage : updated content")
-        else:  # If file doesn't exist, create it
-            self.repository.create_file(path, message, content, branch=branch)  # Add, commit and push branch
+# class github_store:
+#     def __init__(self,file_path=None):
+#         if file_path == None:
+#             self.file_path = "chat_log.txt"
+#         self.token = os.getenv("GITHUB_TOKEN")
+#         self.github_object = Github(self.token)
+#         self.branch = "main"
+#         self.repository = self.github_object.get_repo("marsha-nicky/my_knowledge")
+    
+#     def pull_data(self):
+#         contents = self.repository.get_contents(self.file_path, ref=self.branch)
+#         return contents.decoded_content.decode()
+
+#     def push(self,path=None, message=None, content=None, branch=None, update=False):
+
+#         if path == None:
+#             path = self.file_path
+#         if message == None:
+#             message = "Added chat knowledge"
+#         if content == None:
+#             content = self.data
+#         if branch == None:
+#             branch = self.branch
+
+#         source = self.repository.get_branch(branch)
+#         if update:  # If file already exists, update it
+#             contents = self.repository.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
+#             self.repository.update_file(contents.path, message, content, contents.sha, branch=branch)  # Add, commit and push branch
+#             print("github_storage : updated content")
+#         else:  # If file doesn't exist, create it
+#             self.repository.create_file(path, message, content, branch=branch)  # Add, commit and push branch
